3ad836c1c8f67442156c88b6254a4c87c17b3b45
Zap0xfce2 <>
2025-02-05 16:00:56 +0100

    update Database.py

--- Database.py
+++ Database.py
@@ -3,16 +3,14 @@ import Console
 
 
 def read(tag_id, db_file="NFC-Tags.db"):
-    # Pfad übergeben lassen
     sql = sqlite3.connect(db_file)
     db = sql.cursor()
 
+    Console.info(tag_id)
     db.execute("SELECT command FROM nfc_tags WHERE id=?", tag_id)
     # Console.info(db.fetchone()[0])
     returnvalue = db.fetchone()[0]
+    # Verbindung Schließen
     sql.close()
 
     return returnvalue
-    # print(db.fetchall())
-
-    # Verbindung Schließen
