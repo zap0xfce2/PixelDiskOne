3640a1f3901fb0f396b8ddb247583d9d327d9a55
Zap0xfce2 <>
2025-02-05 16:34:43 +0100

    update Database.py

--- Database.py
+++ Database.py
@@ -7,7 +7,7 @@ def read(tag_id, db_file="NFC-Tags.db"):
     db = sql.cursor()
 
     Console.info(tag_id)
-    suche = (str(tag_id).strip(),)
+    suche = (int(tag_id),)
     db.execute("SELECT command FROM nfc_tags WHERE id=?", suche)
 
     result = db.fetchone()  # Ergebnis abrufen
