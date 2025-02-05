110cbb0c1ff51cf3982b13b2f2ea880693d8d013
Zap0xfce2 <>
2025-02-05 16:20:46 +0100

    update Database.py

--- Database.py
+++ Database.py
@@ -9,9 +9,12 @@ def read(tag_id, db_file="NFC-Tags.db"):
     Console.info(tag_id)
     suche = (tag_id,)
     db.execute("SELECT command FROM nfc_tags WHERE id=?", suche)
-    # Console.info(db.fetchone()[0])
-    returnvalue = db.fetchone()[0]
-    # Verbindung Schließen
-    sql.close()
 
-    return returnvalue
+    result = db.fetchone()  # Ergebnis abrufen
+
+    sql.close()  # Verbindung schließen
+
+    if result:  # Prüfen, ob ein Ergebnis existiert
+        return result[0]
+    else:
+        return None  # Falls kein Eintrag existiert, None zurückgeben oder eine Fehlermeldung ausgeben
