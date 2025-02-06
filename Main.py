e699fbe73f0bdc4cf2a3e98f49921f04039d9d06
Zap0xfce2 <>
2025-02-06 11:18:23 +0100

    update Main.py

--- Main.py
+++ Main.py
@@ -78,11 +78,9 @@ while True:
     else:
         # Falls keine Diskette mehr erkannt wird, beende den laufenden Prozess
         if last_process and last_process.poll() is None:
-            # hier kommt noch das falsche icon ;(
             Notification.send(
                 "Diskette entfernt",
                 "Das Spiel wurde beendet da die Diskette entfernt wurde.",
-                "dialog-information",
             )
             last_process.terminate()
             last_process.wait()
