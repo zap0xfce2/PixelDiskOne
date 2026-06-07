1174d6c428f65681f028df9a21e48855d9f5415e
Zap0xfce2 <902946+zap0xfce2@users.noreply.github.com>
2026-06-07 15:57:06 +0200

    feat: add RetroArchSNES script for joystick configuration

---
+++ RetroArchSNES.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PLAYER1_MAC="e4:17:d8:96:66:69"
+PLAYER2_MAC="e4:17:d8:ae:66:69"
+
+find_index_by_mac() {
+    local target="$1"
+    for js in /dev/input/js*; do
+        local name
+        name=$(basename "$js")
+        local mac
+        mac=$(cat "/sys/class/input/${name}/device/uniq" 2>/dev/null)
+        if [[ "$mac" == "$target" ]]; then
+            echo "${name//[!0-9]/}"
+            return
+        fi
+    done
+    echo ""
+}
+
+first_available_js_index() {
+    local i=0
+    while [[ -e "/dev/input/js$i" ]]; do
+        ((i++))
+    done
+    echo "$i"
+}
+
+P1_IDX=$(find_index_by_mac "$PLAYER1_MAC")
+[[ -z "$P1_IDX" ]] && P1_IDX=$(first_available_js_index)
+
+P2_IDX=$(find_index_by_mac "$PLAYER2_MAC")
+if [[ -z "$P2_IDX" ]]; then
+    NEXT=$((P1_IDX + 1))
+    while [[ -e "/dev/input/js$NEXT" ]]; do ((NEXT++)); done
+    P2_IDX="$NEXT"
+fi
+
+exec retroarch \
+    --input-player1-joypad-index="$P1_IDX" \
+    --input-player2-joypad-index="$P2_IDX" \
+    "$@"
