8498d37056e11b3e88676e8409cbd351e1ffba41
Zap0xfce2 <902946+zap0xfce2@users.noreply.github.com>
2026-07-11 20:43:14 +0200

    fix: uncomment updater and NFC reader reset commands in start script

--- PixelDiskOne-Start.sh
+++ PixelDiskOne-Start.sh
@@ -19,10 +19,10 @@ screen -S MouseHider -dm bash -c "unclutter -idle 0.5 -root"
 rm -f "$HOME/.mute" 2>/dev/null || true
 
 # Updater blockierend ausführen
-#(cd "$HOME/PixelDiskOne" && ./PixelDiskOne-Updater.py)
+(cd "$HOME/PixelDiskOne" && ./PixelDiskOne-Updater.py)
 
 # NFC-Reader-Reset
-#sudo "$HOME/PixelDiskOne/NfcReaderReset.sh"
+sudo "$HOME/PixelDiskOne/NfcReaderReset.sh"
 
 # BGM & Splash Muter starten
 screen -S MuteMusicAndSplash -dm bash -c "cd $HOME/PixelDiskOne && ./MuteMusicAndSplash.sh"
